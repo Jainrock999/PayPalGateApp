5060081dc2e2401a0fefcf4fd067c5fa7a70fc7d
SBS <dodiya.saurav9@gmail.com>
2018-10-04 17:45:34 +0530

    04/10/20185/30

---
+++ app/src/main/java/com/example/sbs/paypalintegrateapp/activity/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.example.sbs.paypalintegrateapp.activity;
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AeUZaaYqjGTo-3Za9ew6QPSSuoNMRKEC9X3srbe505KQ7OBBxz9y00QKVUWnp6heR0EN8_u4Zyr1h7GQ";
+}
